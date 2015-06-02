a2174936d2f7cb4d428a760a68c828e396275270
starkDbl07 <abhi.emailto@gmail.com>
2015-06-02 22:48:59 +0545

    separated server client

--- MouseLocation.m
+++
@@ -1,7 +0,0 @@
-#import <AppKit/AppKit.h>
-
-int main (int argc, const char * argv[]) {
-    NSPoint mouseLoc = [NSEvent mouseLocation];
-    printf("%lu %lu", (unsigned long)mouseLoc.x, ((unsigned long)mouseLoc.y-900) * -1);
-    return 0;
-}
